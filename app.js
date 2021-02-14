617ee1e595b7e6886b8845d2f255a443716fd739
RRanar <kombidemon@gmail.com>
2021-02-14 15:32:12 +0200

    Init project

---
+++ app.js
@@ -0,0 +1,17 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command('start', (ctx) => {
+    ctx.reply(`Hello, ${ctx.message.from.username}`);
+
+   ctx.telegram.sendMessage(process.env.SUPPORT_CHAT_ID, `Walker, ${ctx.message.from.username}, connected`);
+});
+
+bot.on('text', (ctx) => {
+    ctx.telegram.sendMessage(process.env.SUPPORT_CHAT_ID, ctx.message);
+});
+
+bot.command('age', (ctx) => {
+
+});
